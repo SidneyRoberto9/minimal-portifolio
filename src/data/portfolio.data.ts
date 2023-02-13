96c396176844c5968ca74af281c0b53a97a05c8f
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-02-13 12:11:30 -0300

    ✨ feat: Add Others Projects

--- src/data/portfolio.data.ts
+++ src/data/portfolio.data.ts
@@ -2,12 +2,6 @@ import { IPortfolioItem } from '../interfaces/typesPortifolio';
 
 export const portfolioData: IPortfolioItem[] = [
   {
-    title: 'Terminal Website',
-    imgUrl: '/assets/terminal-website.jpg',
-    stack: ['HTML', 'CSS', 'JavaScript'],
-    link: 'https://fkcodes.com',
-  },
-  {
     title: 'Developer Portfolio',
     imgUrl: '/assets/dev-portfolio.jpg',
     stack: ['React', 'Tailwind', 'Vite'],
@@ -18,5 +12,29 @@ export const portfolioData: IPortfolioItem[] = [
     imgUrl: '/assets/tmdb.png',
     stack: ['Angular', 'RXjs', 'SASS'],
     link: 'https://tmdb-desafio-promobit-angular.netlify.app/home',
+  },
+  {
+    title: 'Timer',
+    imgUrl: '/assets/timer.png',
+    stack: ['React', 'Zod', 'React Hook Form', 'Immer'],
+    link: 'https://timer-sid.netlify.app/',
+  },
+  {
+    title: 'Ignite Call',
+    imgUrl: '/assets/call.png',
+    stack: ['Next', 'Next Auth', 'React Query', 'Stitches'],
+    link: 'https://ignite-call-sid.vercel.app/',
+  },
+  {
+    title: 'Generic Landing Page',
+    imgUrl: '/assets/generic-landing-page.jpg',
+    stack: ['React', 'Tailwind', 'Aos'],
+    link: 'https://product-lp.netlify.app/',
+  },
+  {
+    title: 'Ignite Shop',
+    imgUrl: '/assets/ignite-shop.png',
+    stack: ['Next', 'Stripe', 'Stitches', 'Keen slider'],
+    link: 'https://ignite-shop-2.vercel.app/',
   },
 ];
