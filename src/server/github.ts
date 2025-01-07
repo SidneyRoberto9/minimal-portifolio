44daf0d1b403a319f197867c1969f14846fe4c86
Sidney Roberto <sidneyrpsilva@gmail.om>
2025-01-07 00:06:22 -0300

    ✨ feat: limit itens from list to max 9

--- src/server/github.ts
+++ src/server/github.ts
@@ -1,25 +1,26 @@
-import axios from 'axios';
+import axios from "axios"
 
-import { portfolioData } from '@/data/portfolio.data';
-import { GithubRepoResponse } from '@/interfaces/typesGithub';
-import { IPortfolioItem } from '@/interfaces/typesPortfolio';
+import { portfolioData } from "@/data/portfolio.data"
+import { GithubRepoResponse } from "@/interfaces/typesGithub"
+import { IPortfolioItem } from "@/interfaces/typesPortfolio"
 
 export const getGithubRepoData = async (): Promise<IPortfolioItem[]> => {
   try {
     const { data } = await axios.get(
-      'https://api.github.com/users/sidneyroberto9/repos?sort=created&direction=desc&per_page=6',
-    );
-    return filterGithubRepoData(data);
+      "https://api.github.com/users/sidneyroberto9/repos?sort=created&direction=desc&per_page=6"
+    )
+    return filterGithubRepoData(data)
   } catch (error) {
-    console.error('Error fetching GitHub repository data:', error);
+    console.error("Error fetching GitHub repository data:", error)
 
-    return portfolioData;
+    return portfolioData
   }
-};
+}
 
 const filterGithubRepoData = (data: GithubRepoResponse[]) => {
   return data
     .filter((repo) => repo.topics.length > 0)
+    .slice(0, 9)
     .map(
       (repo) =>
         ({
@@ -28,12 +29,12 @@ const filterGithubRepoData = (data: GithubRepoResponse[]) => {
           stack: repo.topics,
           imgUrl: formatGithubProjectImageLink(repo.html_url),
           link: repo.homepage,
-        } as IPortfolioItem),
-    );
-};
+        }) as IPortfolioItem
+    )
+}
 
 const formatGithubProjectImageLink = (url: string) => {
   return url
-    .replace('github.com', 'raw.githubusercontent.com')
-    .concat('/refs/heads/master/.github/1440x1080.png');
-};
+    .replace("github.com", "raw.githubusercontent.com")
+    .concat("/refs/heads/master/.github/1440x1080.png")
+}
